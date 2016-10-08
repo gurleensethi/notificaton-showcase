f3d98497a6cefbfb0f789d081ca9ad46db19712e
gurleensethi <sarusethi@rocketmail.com>
2016-10-08 20:34:14 +0530

    Send notification in onReceive

--- app/src/main/java/app/com/thetechnocafe/notificationshowcase/NotificationPublisher.java
+++ app/src/main/java/app/com/thetechnocafe/notificationshowcase/NotificationPublisher.java
@@ -1,5 +1,7 @@
 package app.com.thetechnocafe.notificationshowcase;
 
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -13,6 +15,18 @@ public class NotificationPublisher extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        //Get notification manager
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 
+        //Get notification
+        Notification notification = intent.getParcelableExtra(NOTIFICATION);
+
+        //Get notification id
+        int id = intent.getParcelableExtra(NOTIFICATION_ID);
+
+        //Send the notification
+        if(notification != null) {
+            notificationManager.notify(id, notification);
+        }
     }
 }
